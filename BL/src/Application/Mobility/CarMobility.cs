851da0e94acc8a44a59f737f87fc924c64961e0e
Jose Culajay <jkmarcosb@gmail.com>
2020-10-26 00:04:34 -0600

    adding mobility application

---
+++ BL/src/Application/Mobility/CarMobility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.src.Application.Mobility
+{
+    public class CarMobility : VehicleMobility
+    {
+        public void SlowDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SpeedUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnOn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BL/src/Application/Mobility/VehicleMobility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.src.Application.Mobility
+{
+    interface VehicleMobility
+    {
+        void SpeedUp();
+        void Stop();
+        void SlowDown();
+        void TurnOn();
+        void TurnOff();
+    }
+}
